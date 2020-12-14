92398caac7930f7383671eaa0300077785c75ecf
othmane el kesri <othmane@moneytrack.io>
2020-12-14 09:55:16 +0100

    fix(blockHead): fix error with the block head offset, when the blockchain just started

--- src/chain/tezos/TezosNodeReader.ts
+++ src/chain/tezos/TezosNodeReader.ts
@@ -82,7 +82,9 @@ export namespace TezosNodeReader {
         if (offset <= 0) { return getBlock(server); }
 
         const head = await getBlock(server);
-        return performGetRequest(server, `chains/${chainid}/blocks/${Number(head['header']['level']) - offset}`).then(json => { return <TezosRPCTypes.TezosBlock>json });
+        const level = Math.max(Number(head['header']['level']) - offset, 0);
+
+        return performGetRequest(server, `chains/${chainid}/blocks/${level}`).then(json => { return <TezosRPCTypes.TezosBlock>json });
     }
 
     /**
@@ -108,7 +110,9 @@ export namespace TezosNodeReader {
         if (offset <= 0) { return getBlockHeader(server); }
 
         const header = await getBlockHeader(server);
-        return performGetRequest(server, `chains/${chainid}/blocks/${Number(header.level) - offset}/header`).then(json => { return <TezosRPCTypes.TezosBlockHeader>json });
+        const level = Math.max(Number(header.level) - offset, 0);
+
+        return performGetRequest(server, `chains/${chainid}/blocks/${level}/header`).then(json => { return <TezosRPCTypes.TezosBlockHeader>json });
     }
 
     /**
